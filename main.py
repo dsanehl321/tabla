b5248a16923260d4e4d5a1f6e35a8beb656d458c
dsanehl321 <124673810+dsanehl321@users.noreply.github.com>
2023-02-07 10:56:09 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.py
@@ -0,0 +1,5 @@
+lista = [[0, 1], [2, 3], [4, 5]]
+
+def on_forever():
+    basic.show_number(0)
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,7 @@
+/**
+ * tabla
+ */
+let lista = [[0, 1], [2, 3], [4, 5]]
+basic.forever(function () {
+    basic.showNumber(lista[2][1])
+})
