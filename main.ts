3ea3b51b3b724656ac665673b30b448676e611d5
dsanehl321 <124673810+dsanehl321@users.noreply.github.com>
2023-02-08 13:14:08 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,17 @@
 /**
  * tabla
  */
+basic.showLeds(`
+    . . # . .
+    . # . # .
+    # . # . #
+    . # . # .
+    # . . . #
+    `)
 let lista = [[0, 1], [2, 3], [4, 5]]
+basic.pause(500)
+basic.clearScreen()
+basic.showString("Hola Mundo")
 basic.forever(function () {
     basic.showNumber(lista[2][1])
 })
